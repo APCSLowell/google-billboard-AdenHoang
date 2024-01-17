daf60aca8b5f1ddeb33670c96da6efa43d74ebbe
AdenHoang <132239401+AdenHoang@users.noreply.github.com>
2024-01-16 17:33:50 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,15 +1,24 @@
+public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+public void setup() {
+    int digitCount = 10;
 
+    for (int i = 2; i <= e.length(); i++) {
+        String digits = e.substring(i, i + digitCount);
+        double dNum = Double.parseDouble(digits);
 
-public class GoogleBillboard {
-	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-	public static void main(final String[] args) {
-		System.out.println("Not the right string, tests will fail!");
-	}
-	
+        if (isPrime(dNum)) {
+            System.out.println(dNum);
+            break;
+        }
+    }
+}
 
-	//Finish this function
-	public boolean isPrime(){
-	}
-
+public boolean isPrime(double num){
+  if(num < 2)
+    return false;
+  for(int i = 2; i <= Math.sqrt(num); i++)
+    if(num % i == 0)
+      return false;
+  return true;
 }
 
